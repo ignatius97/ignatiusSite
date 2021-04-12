8986a2398fae1a6a1fb38e5eaa0c18c037111fe1
Aturinda Ignatius <ignatius.aturinda@gmail.com>
2021-04-12 14:21:52 -0700

    'emailing_function'

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for, request
+from flask.ext.mail import Message, Mail
+import smtplib
+
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+
+
+
+mail = Mail()
 
 app = Flask(__name__)
 
-@app.route('/')
+
+sg = sendgrid.SendGridAPIClient('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')
+myMail = 'ignatius.aturinda@gmail.com'
+
+mail.init_app(app)
+
+@app.route('/', methods =["GET", "POST"])
 def index():
+    if request.method == "POST":
+
+       name = request.form.get("name")
+       email = request.form.get("email")
+       subject = request.form.get("subject")
+       message = request.form.get("message")
+       mail = Mail(myMail, myMail, subject, message)
+       response = sg.client.mail.send.post(request_body=mail.get())
+
+       return 'Form posted.'
     Skills=["Reactjs","Nodejs","Html","CSS","Python Flash","Php","JavaScript"]
     return render_template('index.html',Skills=Skills)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
