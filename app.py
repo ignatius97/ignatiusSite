b3d5dba765c637fde8ee925308bea35147783b11
Aturinda Ignatius <ignatius.aturinda@gmail.com>
2021-04-12 14:28:31 -0700

    'emailing_function_back'

--- app.py
+++ app.py
@@ -2,32 +2,34 @@ from flask import Flask, render_template, url_for, request
 from flask.ext.mail import Message, Mail
 import smtplib
 
-import sendgrid
-import os
-from sendgrid.helpers.mail import *
-
-
-
 mail = Mail()
 
 app = Flask(__name__)
 
 
-sg = sendgrid.SendGridAPIClient('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')
-myMail = 'ignatius.aturinda@gmail.com'
+app.secret_key = 'development key'
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'ignatius.aturinda@gmail.com'
+app.config["MAIL_PASSWORD"] = 'ILuvdagal2u'
 
 mail.init_app(app)
 
 @app.route('/', methods =["GET", "POST"])
 def index():
     if request.method == "POST":
-
        name = request.form.get("name")
        email = request.form.get("email")
        subject = request.form.get("subject")
        message = request.form.get("message")
-       mail = Mail(myMail, myMail, subject, message)
-       response = sg.client.mail.send.post(request_body=mail.get())
+       msg = Message(subject, sender=email, recipients=['ignatius.aturinda@gmail.com'])
+       msg.body = """
+             From: %s <%s>
+             %s
+             """ % (name, email, message)
+       mail.send(msg)
 
        return 'Form posted.'
     Skills=["Reactjs","Nodejs","Html","CSS","Python Flash","Php","JavaScript"]
