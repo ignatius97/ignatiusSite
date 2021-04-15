c639fd7ea137cb1b566c5fd283c68976c4b2ba2f
Aturinda Ignatius <ignatius.aturinda@gmail.com>
2021-04-15 06:26:11 -0700

    emailing working

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
+import smtplib, ssl
 from flask import Flask, render_template, url_for, request
-import sendgrid
-import os
-from sendgrid.helpers.mail import *
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 
 app = Flask(__name__)
-
-myMain = 'ignatius.aturinda@gmail.com'
-
-sg = sendgrid.SendGridAPIClient('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')
-
-
-
 
 @app.route('/', methods =["GET", "POST"])
 def index():
@@ -21,9 +13,43 @@ def index():
        name = request.form.get("name")
        email = request.form.get("email")
        subject = request.form.get("subject")
-       message = request.form.get("message")
-       mail = Mail(myMain, myMain, subject, message)
-       response = sg.client.mail.send.post(request_body=mail.get())
+       msg = request.form.get("message")
+
+       port = 465
+
+       sender = 'ignatius.aturinda@gmail.com'
+       password = 'Iluvdagal2u'
+
+       recieve = sender
+
+       message = """\
+       
+       Message is from {0} @ {1}
+
+       {2} 
+
+       Thank You
+       """.format(name,email,msg)
+
+
+       msg = MIMEMultipart()
+       msg['From'] = sender
+       msg['To'] = recieve
+       msg['Subject'] = subject
+       text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttps://www.python.org"
+
+       part1 = MIMEText(message, 'plain')
+
+       msg.attach(part1)
+
+       context = ssl.create_default_context()
+
+       print("Starting to send")
+       with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+           server.login(sender, password)
+           server.sendmail(sender, recieve, msg.as_string())
+
+       print("sent email!")
 
        return 'Form posted.'
     Skills=["Reactjs","Nodejs","Html","CSS","Python Flash","Php","JavaScript"]
